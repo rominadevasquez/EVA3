607c7d623b54d0693151366904858cc68690461e
Romina De Vasquez <rominadevasquez@gmail.com>
2026-05-17 11:17:59 -0400

    codigo header

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,10 @@
+nst Header = () => {
+ return (
+ <header className="header">
+ <h1>Gestor de Cursos React</h1>
+ <p>SPA con API, LocalStorage, componentes y buenas prácticas de
+seguridad.</p>
+ </header>
+ );
+};
+export default Header;
